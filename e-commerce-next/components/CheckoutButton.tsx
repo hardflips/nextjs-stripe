038fdb4772d3b9a3bae93db25d7b2402806c893a
Fábio Lauratto <flipshardflips@hotmail.com>
2021-03-14 23:53:05 -0300

    fix: change  env variables

--- e-commerce-next/components/CheckoutButton.tsx
+++ e-commerce-next/components/CheckoutButton.tsx
@@ -22,8 +22,8 @@ const CheckoutButton: React.FC<Props> = ({ priceId, itemName }) => {
         }
       ],
       mode: 'payment',
-      successUrl: `${process.env.VERCEL_URL}/success?itemName=${itemName}`,
-      cancelUrl: `${process.env.VERCEL_URL}/cancel`,
+      successUrl: `https://${process.env.VERCEL_URL}/success?itemName=${itemName}`,
+      cancelUrl: `https://${process.env.VERCEL_URL}/cancel`,
     });
     if (error) {
       console.log(error);
